ed315150b458f38ca1ed30795c5e4756977181c8
edzempel <edward.zempel@gmail.com>
2019-10-28 23:40:34 -0500

    add CombinationEntry

---
+++ src/edu/metrostate/by8477ks/atm/CombinationEntry.java
@@ -0,0 +1,37 @@
+package edu.metrostate.by8477ks.atm;
+
+public class CombinationEntry {
+    private int amount;
+    private int combinations;
+    private long duration;
+
+    public CombinationEntry(int amount, int combinations, long duration) {
+        this.amount = amount;
+        this.combinations = combinations;
+        this.duration = duration;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public int getCombinations() {
+        return combinations;
+    }
+
+    public void setCombinations(int combinations) {
+        this.combinations = combinations;
+    }
+
+    public long getDuration() {
+        return duration;
+    }
+
+    public void setDuration(long duration) {
+        this.duration = duration;
+    }
+}
